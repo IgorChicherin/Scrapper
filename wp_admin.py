de9173b695778a92e8a078f1d659431f8804868a
ashzak990@gmail.com <ashzak990@gmail.com>
2017-07-04 17:56:41 +0300

    не работает добавление размеров в mysql

--- wp_admin.py
+++ wp_admin.py
@@ -2,16 +2,31 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-session = requests.Session()
-payload = {'log': 'olegsent', 'pwd': 'dW#h$x*^EnGOpLhH'}
-r = session.post('http://localhost/wp-login.php', payload)
-r = session.get('http://localhost/wp-admin/post.php?post=13315&action=edit')
-soup = BeautifulSoup(r.text, 'lxml')
-script_block = soup.findAll('script')
-for item in script_block:
-    security_id = re.search(r'(?<=security: \')(\w+)', item.text)
-    if security_id is not None:
-        security = security_id.group(0)
-payload = {'action': 'woocommerce_add_variation', 'post_id': '13315', 'loop': '6', 'security': security}
-r = session.post('http://localhost/wp-admin/admin-ajax.php', payload)
-print(r.text)
+with requests.Session() as session:
+    payload = {'log': 'olegsent', 'pwd': 'dW#h$x*^EnGOpLhH'}
+    r = session.post('http://localhost/wp-login.php', payload)
+    r = session.get('http://localhost/wp-admin/post.php?post=13315&action=edit')
+    soup = BeautifulSoup(r.text, 'lxml')
+    script_block = soup.findAll('script')
+    for item in script_block:
+        security_id = re.search(r'(?<=security: \')(\w+)', item.text)
+        if security_id is not None:
+            security = security_id.group(0)
+    data = {'action': 'woocommerce_add_variation', 'post_id': '13315', 'loop': '6', 'security': security}
+    payload2 = {'attribute_pa_size[6]': '66', 'variable_post_id[6]': '15779', 'variation_menu_order[6]': '1',
+                'upload_image_id[6]': '0', 'variable_sku[6]': '', 'variable_enabled[6]': 'on',
+                'variable_regular_price[6]': '', 'variable_sale_price[6]': '', 'variable_sale_price_dates_from[6]': '',
+                'variable_sale_price_dates_to[6]': '', 'variable_stock[6]': '', 'variable_backorders[6]': '',
+                'variable_stock_status[6]': '', 'variable_weight[6]': '', 'variable_length[6]': '', 'variable_width[6]': '',
+                'variable_height[6]': '', 'variable_shipping_class[6]': '-1', 'variable_description[6]': '',
+                'variable_download_limit[6]': '', 'variable_download_expiry[6]': '', 'default_attribute_pa_size': '',
+                'action': 'woocommerce_save_variations', 'security': security, 'product_id': '13315',
+                'product-type': 'variable'
+                }
+    payload = {'post': data}
+    # r = session.post('http://localhost/wp-admin/admin-ajax.php', payload)
+    r = session.post('http://localhost/wp-admin/post.php?post=13315&action=edit', payload)
+    # r = session.post('http://localhost/wp-admin/admin-ajax.php', data=payload2)
+    # with open('res.html', 'w', encoding='utf-8') as file:
+    #     file.write(r.text)
+    print(r.cookies)
